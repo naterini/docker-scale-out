naterini/docker-scale-out

commit dd9544ae72927445f49923c7b096e236aa9565ad

Correct calling of docker-compose for nocache target

Makefile
@@ -1,19 +1,20 @@
 HOST ?= mgmtnode
-
+BUILD ?= up --build --remove-orphans -d
 default: run
 
 ./docker-compose.yml: buildout.sh
 	bash buildout.sh > ./docker-compose.yml
 
 build: ./docker-compose.yml
-	env COMPOSE_HTTP_TIMEOUT=3000 docker-compose up --build --remove-orphans -d
+	env COMPOSE_HTTP_TIMEOUT=3000 docker-compose $(BUILD)
 
 stop:
 	docker-compose down
 
-nocache:
-	unlink docker-compose.yml
-	docker-compose build --no-cache
+set_nocache:
+BUILD := build --no-cache
+
+nocache: set_nocache build
 
 clean:
 	docker-compose down --remove-orphans -t1 -v

commit 22544f188191a4cb0dfe5a90f933d26e2070e8e5

Correct BUILD to only change when nocache is requested
Regression in dd9544ae72

Makefile
@@ -12,7 +12,7 @@ stop:
 	docker-compose down
 
 set_nocache:
-BUILD := build --no-cache
+	$(eval BUILD := build --no-cache)
 
 nocache: set_nocache build
 