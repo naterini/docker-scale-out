b99b20532359b0e7b88fe6581bb7d56d27c800ba
Nate Rini <nate@schedmd.com>
2021-01-11 12:00:01 -0700

    Add systemd setup files
    Cont of  5912dfb9ff7a

---
+++ scaleout/slurmctld.startup2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while true
+do
+	#wait until config is filled out by primary before starting
+	grep node00 /etc/slurm/slurm.conf 2>&1 >/dev/null
+	[ $? -eq 0 ] && sleep 1 && break
+	sleep 0.25
+done
+
+scontrol token username=slurm lifespan=9999999 | sed 's#SLURM_JWT=##g' > /auth/slurm
