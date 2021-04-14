681c2a2211e2f34c523e8acd3eb37615fefb0b52
Nate Rini <nate@schedmd.com>
2021-04-13 19:54:23 -0600

    Add enroot and pyxis

--- scaleout/startup.sh
+++ scaleout/startup.sh
@@ -3,5 +3,8 @@
 # Add hosts in the not crazy slow manner
 cat /etc/hosts.nodes >> /etc/hosts
 
+#ensure the systemd cgroup directory exists for enroot
+mkdir -p $(awk -F: '$2 ~ /systemd/ {printf "/sys/fs/cgroup/systemd/%s", $3}' /proc/self/cgroup)
+
 #start systemd
 exec /lib/systemd/systemd --system --log-level=info --crash-reboot --log-target=journal-or-kmsg
