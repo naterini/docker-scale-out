924014361630896380cd3d99f69b8767547d6524
Nate Rini <nate@schedmd.com>
2021-04-15 10:16:14 -0600

    Update wait for nodes.conf to be filled for slurmrestd

--- scaleout/slurmrestd.startup.sh
+++ scaleout/slurmrestd.startup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-while true
+#wait until config is filled out by controller before starting
+while [ ! -s /etc/slurm/nodes.conf ]
 do
-	#wait until config is filled out by controller before starting
-	grep node00 /etc/slurm/slurm.conf 2>&1 >/dev/null
-	[ $? -eq 0 ] && sleep 1 && break
-	sleep 0.25
+		sleep 0.25
 done
 
 exit 0
