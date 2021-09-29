838a490794d107d737e198d8b3f0c4e22e29a3f8
Shawn Hoopes <shawn@schedmd.com>
2021-09-29 16:04:02 -0600

    Update Lab scripts for 21.08

--- scaleout/lab_scripts/showjob.py
+++ scaleout/lab_scripts/showjob.py
@@ -5,7 +5,7 @@ import sys
 import openapi_client
 from openapi_client.rest import ApiException
 from pprint import pprint
-# Defining the host is optional and defaults to http://localhost/slurm/v0.0.36
+# Defining the host is optional and defaults to http://localhost/slurm/v0.0.37
 # See configuration.py for a list of all supported configuration parameters.
 configuration = openapi_client.Configuration(
     host = "http://rest/",
@@ -14,8 +14,8 @@ configuration = openapi_client.Configuration(
         "X-SLURM-USER-TOKEN": os.environ['SLURM_JWT']
     }
 )
-from openapi_client.models import V0036JobSubmission as jobSubmission
-from openapi_client.models import V0036JobProperties as jobProperties
+from openapi_client.models import V0037JobSubmission as jobSubmission
+from openapi_client.models import V0037JobProperties as jobProperties
 from openapi_client.api import SlurmApi as slurm
 # Create an instance of the API class
 s = slurm(openapi_client.ApiClient(configuration))
