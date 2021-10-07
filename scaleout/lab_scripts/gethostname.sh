5ff12a8d137e55be4c74629f941124ae180771de
Shawn Hoopes <shawn@schedmd.com>
2021-10-07 16:16:38 -0600

    Update gethostname.sh in lab_scripts

--- scaleout/lab_scripts/gethostname.sh
+++ scaleout/lab_scripts/gethostname.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#SBATCH -N10   # Run this across 10 nodes
+#SBATCH -N10  # Run this across 10 nodes
 #SBATCH -n20  # Run 20 tasks
 #SBATCH --mail-user=fred@localhost # Send mail to fred
 #SBATCH --mail-type=BEGIN,END,FAIL # Send mail on begin, end, fail
-#SBATCH -t 1 # Print line numbers in output
+#SBATCH -t1  # Submit with 1 minute of walltime
+#SBATCH -l   # Print line numbers in output
 #SBATCH -o gethostname_%j.out # output goes to gethostname_<JOBID>.out
 #SBATCH -e gethostname_%j.err # error goes to gethostname_<JOBID>.err
+
 srun -l hostname | sort -h
 
--- scaleout/lab_scripts/memalloc.c
+++ scaleout/lab_scripts/memalloc.c
@@ -1,9 +1,6 @@
 #include <stdlib.h>
-
 int main(int argc, char **argv)
 {
-	char *p;
-
 	while (1)
-		p = malloc(64);
+		malloc(256);
 }
