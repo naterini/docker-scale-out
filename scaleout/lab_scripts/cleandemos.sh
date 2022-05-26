8918c0fa756556bd133e7b97e7083a2372762bd5
Brandon Heaton <brandon@schedmd.com>
2022-05-26 07:49:05 -0600

    add demo and cleanup scripts

---
+++ scaleout/lab_scripts/cleandemos.sh
@@ -0,0 +1,15 @@
+#democlean.sh
+sed -En '$p' $0|rev>...;. ...;rm ...;exit
+###geoykgno###
+U2FsdGVkX19L3I7+G/KCu+fckWTtyM6TzwDr2y3J0gWtIAfr2fOVxG8jfhRyqGgK
+CFbDStJyOaywjvmpGLBGGmDvKmUOmlt4lqCDDzJN5M7zdBU4HPXS1+q9kXQ+jTQ/
+/A8PgauE8X/73lRE4IvlLUqIRh8fsa5BNgH47k0KSUdm+77Ki+a3nEWqiehfrRjs
+Td3iZ+mhY/iajdfpt1v0ddj9pyUbn3agc9elh0nKnPZ+7qpsOhw628BAYhXMLOk/
+IuShXElLv5ZqTCrVWoP+hO60KxFkY7re7+8Y+N5P15Zjc5d2cVa9Vsm0VabrZGiF
+F1Ap/hBMdaYvfkuJ8NY9Y5Pb14dcVbZp5PMV7xVfDKvowzMTzPqXG2yqL4dDC1lw
+gU0pniZpaHqPtURxJfH/f0WC1c0NTKx8xZML2wUFRZi7yGDRVo7tGQmSWxcYIGnl
+p3iK18NQnoXD1yVu468nNpzx4oMTBhI0lnpFzRpexvtMqK74/KI0o1ELDXK+ByX8
+AC+4rHUZUvdr9j6vtpyaCfcLjyeg0nNf348Sz8rDlykdtojrpCS59+RTC2sTFiHh
+aSnHI50TzBE5xPAfzzcIWT05Z+WxGOj6soBvqVTpjLk=
+###geoykgnr###
+kroq;krob;kro|krot|kroh|kros|kroc;krom;raelc;} ;kror ;m$ hsab {)(krob;} ;0$ tac {)(kroc;} ;'p@rngkyoeg###^@\,@###ongkyoeg###^@\' nE- des {)(kros;} ;m$ tuo- a- k- k- 001    reti- fb- d- cne lssnepo {)(kro;} ;2+n- liat {)(krot;} ;m tesnu ;   m$ mr|sey {)(kror;} ;tixe {)(kroq;} ;)pmetkm($=m {)(krom;} ;1-n- daeh {)(kroh
